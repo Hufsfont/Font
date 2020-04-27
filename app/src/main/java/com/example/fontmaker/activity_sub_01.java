20e21a288a476469ce36dddc086a4be247223ffa
mulgyeol <mulgyeoljung@gmail.com>
2020-04-27 19:37:09 +0900

    add camera_test02

--- app/src/main/java/com/example/fontmaker/activity_sub_01.java
+++ app/src/main/java/com/example/fontmaker/activity_sub_01.java
@@ -1,6 +1,8 @@
 package com.example.fontmaker;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -23,6 +25,8 @@ public class activity_sub_01 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_01);
+
+        AutoPermissions.Companion.loadAllPermissions(this,101);
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -51,12 +55,26 @@ public class activity_sub_01 extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onAvtivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 101 && resultCode == RESULT_OK){
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 8;
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(),options);
+
+            imageView.setImageBitmap(bitmap);
+        }
+
+    }
+
     public void takePickture(){
         if (file == null){
             file = createFile();
         }
 
-        Uri fileUri = FileProvider.getUriForFile(this,"org.techtown.capute.intetn.fileprovider", file);
+        Uri fileUri = FileProvider.getUriForFile(this,"com.example.fontmaker", file);
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
         if (intent.resolveActivity(getPackageManager()) != null){
