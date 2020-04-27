c7676f4d9765f3e90d77b79100d3fccf54828d7f
mulgyeol <mulgyeoljung@gmail.com>
2020-04-27 18:54:04 +0900

    add camera_test01

--- app/src/main/java/com/example/fontmaker/activity_sub_01.java
+++ app/src/main/java/com/example/fontmaker/activity_sub_01.java
@@ -1,13 +1,23 @@
 package com.example.fontmaker;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
 
 public class activity_sub_01 extends AppCompatActivity {
+    ImageView imageView;
+    File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +39,36 @@ public class activity_sub_01 extends AppCompatActivity {
                 startActivityForResult(intent, 1);
             }
         });
+
+        imageView = findViewById(R.id.imageView);
+
+        ImageButton btn_camera = findViewById(R.id.btn_camera);
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePickture();
+            }
+        });
+    }
+
+    public void takePickture(){
+        if (file == null){
+            file = createFile();
+        }
+
+        Uri fileUri = FileProvider.getUriForFile(this,"org.techtown.capute.intetn.fileprovider", file);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, 101);
+        }
+    }
+
+    private File createFile(){
+        String filename = "capture.jpg";
+        File storageDir = Environment.getExternalStorageDirectory();
+        File outFile = new File(storageDir, filename);
+
+        return outFile;
     }
 }
