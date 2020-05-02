a2b9cb328576bdd46b1efdaceb0e73c607d3371e
mulgyeol <mulgyeoljung@gmail.com>
2020-05-03 02:36:34 +0900

    add camera and crop api

--- app/src/main/java/com/example/fontmaker/activity_sub_01.java
+++ app/src/main/java/com/example/fontmaker/activity_sub_01.java
@@ -1,30 +1,48 @@
 package com.example.fontmaker;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
-import com.pedro.library.AutoPermissions;
-import com.pedro.library.AutoPermissionsListener;
+import com.example.fontmaker.enums.ImagePickerEnum;
+import com.example.fontmaker.listeners.IImagePickerLister;
+import com.example.fontmaker.utils.FileUtils;
+import com.example.fontmaker.utils.UiHelper;
+import com.yalantis.ucrop.UCrop;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-public class activity_sub_01 extends AppCompatActivity implements AutoPermissionsListener {
-    ImageView imageView;
-    File file;
+
+public class activity_sub_01 extends AppCompatActivity implements IImagePickerLister {
+
+    private static final int CAMERA_ACTION_PICK_REQUEST_CODE = 610;
+    private static final int PICK_IMAGE_GALLERY_REQUEST_CODE = 609;
+    public static final int CAMERA_STORAGE_REQUEST_CODE = 611;
+    public static final int ONLY_CAMERA_REQUEST_CODE = 612;
+    public static final int ONLY_STORAGE_REQUEST_CODE = 613;
+
+    private String currentPhotoPath = "";
+    private UiHelper uiHelper = new UiHelper();
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +50,6 @@ public class activity_sub_01 extends AppCompatActivity implements AutoPermission
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_01);
-
-        AutoPermissions.Companion.loadAllPermissions(this,101);
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -43,74 +59,159 @@ public class activity_sub_01 extends AppCompatActivity implements AutoPermission
         });
 
         Button button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener(){
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), activity_sub_02.class);
                 startActivityForResult(intent, 2);
             }
         });
 
-        imageView = findViewById(R.id.imageView);
-
-        ImageButton btn_camera = findViewById(R.id.btn_camera);
-        btn_camera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                takePickture();
-            }
+        findViewById(R.id.btn_camera).setOnClickListener(v -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                if (uiHelper.checkSelfPermissions(this))
+                    uiHelper.showImagePickerDialog(this, this);
         });
+
+        imageView = findViewById(R.id.imageView);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 101 && resultCode == RESULT_OK){
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = 8;
-            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(),options);
-
-            imageView.setImageBitmap(bitmap);
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (requestCode == CAMERA_STORAGE_REQUEST_CODE) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED)
+                    uiHelper.showImagePickerDialog(this, this);
+                else if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_DENIED) {
+                    uiHelper.toast(this, "ImageCropper needs Storage access in order to store your profile picture.");
+                    finish();
+                } else if (grantResults[0] == PackageManager.PERMISSION_DENIED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    uiHelper.toast(this, "ImageCropper needs Camera access in order to take profile picture.");
+                    finish();
+                } else if (grantResults[0] == PackageManager.PERMISSION_DENIED && grantResults[1] == PackageManager.PERMISSION_DENIED) {
+                    uiHelper.toast(this, "ImageCropper needs Camera and Storage access in order to take profile picture.");
+                    finish();
+                }
+            } else if (requestCode == ONLY_CAMERA_REQUEST_CODE) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    uiHelper.showImagePickerDialog(this, this);
+                else {
+                    uiHelper.toast(this, "ImageCropper needs Camera access in order to take profile picture.");
+                    finish();
+                }
+            } else if (requestCode == ONLY_STORAGE_REQUEST_CODE) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    uiHelper.showImagePickerDialog(this, this);
+                else {
+                    uiHelper.toast(this, "ImageCropper needs Storage access in order to store your profile picture.");
+                    finish();
+                }
+            }
         }
 
-    }
-
-    public void takePickture(){
-        if (file == null){
-            file = createFile();
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == CAMERA_ACTION_PICK_REQUEST_CODE && resultCode == RESULT_OK) {
+                Uri uri = Uri.parse(currentPhotoPath);
+                openCropActivity(uri, uri);
+            } else if (requestCode == UCrop.REQUEST_CROP && resultCode == RESULT_OK) {
+                if (data != null) {
+                    Uri uri = UCrop.getOutput(data);
+                    showImage(uri);
+                }
+            } else if (requestCode == PICK_IMAGE_GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+                try {
+                    Uri sourceUri = data.getData();
+                    File file = getImageFile();
+                    Uri destinationUri = Uri.fromFile(file);
+                    openCropActivity(sourceUri, destinationUri);
+                } catch (Exception e) {
+                    uiHelper.toast(this, "Please select another image");
+                }
+            }
         }
 
-        Uri fileUri = FileProvider.getUriForFile(this,"com.example.fontmaker.fileprovider",file);
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-        if (intent.resolveActivity(getPackageManager()) != null){
-            startActivityForResult(intent, 101);
+        private void openImagesDocument() {
+            Intent pictureIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            pictureIntent.setType("image/*");
+            pictureIntent.addCategory(Intent.CATEGORY_OPENABLE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                String[] mimeTypes = new String[]{"image/jpeg", "image/png"};
+                pictureIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+            }
+            startActivityForResult(Intent.createChooser(pictureIntent, "Select Picture"), PICK_IMAGE_GALLERY_REQUEST_CODE);
         }
+
+        private void showImage(Uri imageUri) {
+            try {
+                File file;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                    file = FileUtils.getFile(this, imageUri);
+                } else {
+                    file = new File(currentPhotoPath);
+                }
+                InputStream inputStream = new FileInputStream(file);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                uiHelper.toast(this, "Please select different profile picture.");
+            }
+        }
+
+        @Override
+        public void onOptionSelected(ImagePickerEnum imagePickerEnum) {
+            if (imagePickerEnum == ImagePickerEnum.FROM_CAMERA)
+                openCamera();
+            else if (imagePickerEnum == ImagePickerEnum.FROM_GALLERY)
+                openImagesDocument();
+        }
+
+        private void openCamera() {
+            Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            File file;
+            try {
+                file = getImageFile(); // 1
+            } catch (Exception e) {
+                e.printStackTrace();
+                uiHelper.toast(this, "Please take another image");
+                return;
+            }
+            Uri uri;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) // 2
+                uri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID.concat(".provider"), file);
+            else
+                uri = Uri.fromFile(file); // 3
+            pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri); // 4
+            startActivityForResult(pictureIntent, CAMERA_ACTION_PICK_REQUEST_CODE);
+        }
+
+    private File getImageFile() throws IOException {
+        String imageFileName = "JPEG_" + System.currentTimeMillis() + "_";
+        File storageDir = new File(
+                Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_DCIM
+                ), "Camera"
+        );
+        System.out.println(storageDir.getAbsolutePath());
+        if (storageDir.exists())
+            System.out.println("File exists");
+        else
+            System.out.println("File not exists");
+        File file = File.createTempFile(
+                imageFileName, ".jpg", storageDir
+        );
+        currentPhotoPath = "file:" + file.getAbsolutePath();
+        return file;
     }
 
-    private File createFile(){
-        String filename = "capture.jpg";
-        File storageDir = Environment.getExternalStorageDirectory();
-        File outFile = new File(storageDir, filename);
-
-        return outFile;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        AutoPermissions.Companion.parsePermissions(this, requestCode, permissions, this);
-    }
-
-    @Override
-    public void onDenied(int requestCode, String[] permissions){
-        Toast.makeText(this, "permissions denied : " + permissions.length, Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onGranted(int requestCode, String[] permissions){
-        Toast.makeText(this, "permissions granted : " + permissions.length, Toast.LENGTH_LONG).show();
-    }
-
+        private void openCropActivity(Uri sourceUri, Uri destinationUri) {
+            UCrop.Options options = new UCrop.Options();
+            options.setCircleDimmedLayer(true);
+            options.setCropFrameColor(ContextCompat.getColor(this, R.color.colorAccent));
+            UCrop.of(sourceUri, destinationUri)
+                    .withMaxResultSize(500, 100)
+                    .withAspectRatio(5f, 1f)
+                    .start(this);
+        }
 }
